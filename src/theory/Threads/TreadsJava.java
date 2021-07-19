3c8a4524cd15ffe334fed644ccdf67fb630e1d9c
gretaivan <greta.ivan91@gmail.com>
2021-07-19 12:26:34 +0100

    completes basic threading implementation

---
+++ src/theory/Threads/TreadsJava.java
@@ -0,0 +1,18 @@
+package theory.Threads;
+
+public class TreadsJava {
+
+	public static void main(String[] args) {
+		Thread getTime = new GetTime20(); 
+		
+		Runnable getMail = new GetTheMail(5);
+		Runnable getMailAgain = new GetTheMail(10);
+		
+		getTime.start();
+		
+		new Thread(getMail).start(); 
+		new Thread(getMailAgain).start(); 
+
+	}
+
+}

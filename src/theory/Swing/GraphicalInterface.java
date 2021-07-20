36082c86e9f43eac93846e5e21d4980e9d6a3eef
gretaivan <greta.ivan91@gmail.com>
2021-07-20 09:28:36 +0100

    adds the GUI with Swing and implements basic window

---
+++ src/theory/Swing/GraphicalInterface.java
@@ -0,0 +1,36 @@
+package theory.Swing;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import javax.swing.*;
+
+
+public class GraphicalInterface extends JFrame{
+
+	public static void main(String[] args) {
+		new GraphicalInterface(); 
+	}
+
+	public GraphicalInterface() {
+		this.setSize(400, 400);
+		this.setVisible(true);
+		
+		//change the location on the screen e.g. center
+		this.setLocationRelativeTo(null);
+	
+		//abstract class of window toolkit, that enables communication with OS
+		Toolkit tk = Toolkit.getDefaultToolkit();
+		
+		Dimension dim = tk.getScreenSize();
+		
+		// center everything 
+		int xPos = (dim.width / 2) - (this.getWidth() / 2 ); 
+		int yPos = (dim.height / 2) - (this.getHeight() / 2); 
+
+		this.setLocation(xPos, yPos); 
+		
+		//dont allow resizing
+		this.setResizable(false);
+	}
+	
+}

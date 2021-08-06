4d660c5b457fe7311ff0a4746c9a3d961b88bbbd
gretaivan <greta.ivan91@gmail.com>
2021-08-06 14:24:40 +0100

    refactors comparable to java generics

--- src/interfaces/Passenger.java
+++ src/interfaces/Passenger.java
@@ -22,3 +22,28 @@ public class Passenger implements Comparable {
 	}
 	
 }
+
+//Java generics refactoring
+/*public class Passenger implements Comparable<Passenger> {
+	private String name; 
+	private int memberLevel; // 3 (1st priority), 2, 1
+	private int memberDays; 
+	
+	public Passenger(String name, int memberLevel, int memberDays) {
+		this.name = name; 
+		this.memberLevel = memberLevel; 
+		this.memberDays = memberDays; 
+	}
+	
+	//INTERFACE METHOD: order members on the membership level
+	//the rule is to return -1|0|1 
+	public int compareTo(Passenger p) {
+		
+		int returnValue = p.memberLevel - memberLevel; 
+		if(returnValue == 0) {
+			returnValue = p.memberDays - memberDays; 
+		}
+		return returnValue; 
+	}
+	
+}*/

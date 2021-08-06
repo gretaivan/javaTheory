c827c23f158178301d921e386b3f8bb9ce4ad1ce
gretaivan <greta.ivan91@gmail.com>
2021-08-06 18:29:58 +0100

    adds anonymous class creation and implementation in Flight class

--- src/interfaces/Flight.java
+++ src/interfaces/Flight.java
@@ -25,24 +25,142 @@ public class Flight
         this.flightClass = flightClass;
     }
     
-    //generic type interface example
-	public Iterable<Passenger> getOrderedPassengers() {
-        return new Iterable<Passenger>() {
-            @Override
-            public Iterator<Passenger> iterator() {
-                Passenger[] passengers = new Passenger[passengerList.size()];
-                passengerList.toArray(passengers);
-                Arrays.sort(passengers);
-                return Arrays.asList(passengers).iterator();
-            }
-        };
+    //generic type interface example for iterator
+    @Override
+    public Iterator<Passenger> iterator() {
+        return passengerList.iterator();
     }
+    
+    /* To use iterator above the implementation of Iterable is needed typically done in
+    *  an inner class
+    * But can be implemented with anonymous class
+    */
+    
+   //Inner class example
+   /*
+      private class FlightIterable implements Iterable<Passenger> {
+     	public Iterator<Passenger> iterator() {
+    		Passenger[] passengers = new Passenger[passengerList.size()];
+    		passengerList.toArray(passengers); 
+    		Arrays.sort(passengers); 
+    		return Arrays.asList(passengers).iterator();
+    	}
+    }
+    */
+    
+	//Anonymous class implementation
+	  public Iterable<Passenger> getOrderedPassengers() { 
+		  return new Iterable<Passenger>() {  //implements iterable interface
+		  //method
+		  @Override 
+		  public Iterator<Passenger> iterator() { 
+			  Passenger[] passengers = new Passenger[passengerList.size()]; 
+			  passengerList.toArray(passengers);
+			  Arrays.sort(passengers); 
+			  return Arrays.asList(passengers).iterator(); 
+			  } 
+		  }; //end of anonymous class instanciation
+	  }
+	 
 	
+	
+	
+	//sort according to the filght time
 	@Override
-	public int compareTo(Flight o) {
-		// TODO Auto-generated method stub
-		return 0;
+	public int compareTo(Flight flight) {
+		return flightTime - flight.flightTime;
 	}
     
+	//functionality methods
+	
+	private boolean hasSeating() {
+        return passengers < seats;
+	}
+	
+	private boolean hasSeating(int count) {
+	   return passengers + count <= seats;
+	}	
+	
+	//method chaining
+	public void addPassenger() {
+
+        if(hasSeating())
+            passengers += 1;
+    }
+	
+    public void addPassenger(int bags) {
+
+        if (hasSeating()) {
+            addPassenger();
+            totalCheckedBags += bags;
+        }
+    }
+    
+    public void addPassenger(Passenger p) {
+
+        passengerList.add(p);
+        addPassenger(p.getCheckedBags());
+    }
+    
+    public void addPassenger(int bags, int carryOns) {
+
+        if(carryOns <= 2)
+            addPassenger(bags);
+    }
+    
+	public void addPassenger(Passenger p, int carryOns) {
+
+        passengerList.add(p);
+        addPassenger(p.getCheckedBags(), carryOns);
+    }
+
+    public void addPassengers(Passenger... list) {
+
+        if (hasSeating(list.length)) {
+            passengers += list.length;
+            for (Passenger passenger : list) {
+                passengerList.add(passenger);
+                totalCheckedBags += passenger.getCheckedBags();
+            }
+        }
+    }
+	
+	//getters and setters
+	
+	public int getPassengers() {
+        return passengers;
+    }
+
+    public void setPassengers(int passengers) {
+        this.passengers = passengers;
+    }
+
+    public int getSeats() {
+        return seats;
+    }
+
+    public void setSeats(int seats) {
+        this.seats = seats;
+    }
+
+    public int getFlightNumber() {
+        return flightNumber;
+    }
+
+    public void setFlightNumber(int flightNumber) {
+        this.flightNumber = flightNumber;
+    }
+
+    public int getFlightTime() {
+        return flightTime;
+    }
+
+    public void setFlightTime(int flightTime) {
+        this.flightTime = flightTime;
+    }
+
+    public int getTotalCheckedBags() {
+        return totalCheckedBags;
+    }
     
 }
--- src/interfaces/Main.java
+++ src/interfaces/Main.java
@@ -15,9 +15,32 @@ public class Main {
 				new Passenger("Ashanti", 3, 730),
 				new Passenger("Harish", 2, 150)
 		};
-
+		
+		f175.addPassenger(new Passenger("Luisa", 1, 180));
+        f175.addPassenger(new Passenger("Jack", 1, 90));
+        f175.addPassenger(new Passenger("Ashanti", 3, 730));
+        f175.addPassenger(new Passenger("Harish", 2, 150));
+        
+        System.out.println(" ** Unordered passengers ** ");
+		for(Passenger p : f175)
+            System.out.println(p.getName());
+		
+		
 		//interface method implementation allows sorting the array
-		Arrays.sort(passengers);
+//		Arrays.sort(passengers); 
+//		System.out.println("*****Ordered passengers*****");
+//		for(Passenger p : f175)
+//            System.out.println(p.getName());
+		
+		System.out.println(" ** Passenger order using Flight class' Iterable implementation **");
+        for(Passenger p : f175)
+            System.out.println(p.getName());
+        
+        System.out.println(" ** Passenger order using the anonymous class' Iterable implementation **");
+        for(Passenger p : f175.getOrderedPassengers())
+            System.out.println(p.getName());
+
+    
 	
 		
 		
